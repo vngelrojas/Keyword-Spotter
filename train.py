b5cf52e385749551e037f9a26340f672cce8b14c
Angel Rojas <aarojas@usc.edu>
2023-09-15 23:42:42 -0700

    Implemented buildModel funciton

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
-import keras
 import json
 import numpy as np
+import keras as keras
 from sklearn.model_selection import train_test_split
 
 DATA_PATH = "data.json"
@@ -8,6 +8,7 @@ SAVED_MODEL_PATH = "model.h5"
 LEARNING_RATE = 0.0001
 EPOCHS = 40 # number of times data is passed through the network for training
 BATCH_SIZE = 32
+NUM_KEYWORDS = 10
 
 def loadDataset(data_path):
     with open(data_path,"r") as fp:
@@ -35,12 +36,50 @@ def getDataSplits(data_path,test_size=0.1,test_validation=0.1):
     
     return X_train,X_validation,X_test,y_train,y_validation,y_test
 
+def buildModel(input_shape,learning_rate,error="sparse_categorical_crossentropy"):
+    # build network
+    model = keras.Sequential()
+    
+    # conv layer 1
+    model.add(keras.layers.Conv2D(64,(3,3), activation="relu", input_shape=input_shape,kernel_regularizer=keras.regularizers.l2(0.001)))
+    model.add(keras.layers.BatchNormalization())
+    # down samples (by a factor of 2) output of convolutional layer
+    model.add(keras.layers.MaxPool2D((3,3), strides=(2,2), padding="same"))
+    
+    # conv layer 2
+    model.add(keras.layers.Conv2D(32,(3,3), activation="relu",kernel_regularizer=keras.regularizers.l2(0.001))) 
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.MaxPool2D((3,3), strides=(2,2), padding="same"))
+    
+    # conv layer 3
+    model.add(keras.layers.Conv2D(32,(2,2), activation="relu",kernel_regularizer=keras.regularizers.l2(0.001)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.MaxPool2D((2,2), strides=(2,2), padding="same"))
+    
+    # flatten the output and feed it into a dense layer
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(64, activation="relu"))
+    model.add(keras.layers.Dropout(0.3))
+    
+    # softmax classifier
+    model.add(keras.layers.Dense(NUM_KEYWORDS,activation="softmax"))
+    
+    # complie the model
+    optimiser = keras.optimizers.Adam(learning_rate=learning_rate)
+    model.compile(optimizer=optimiser, loss=error,metrics=["accuracy"])
+    
+    # print model overview
+    model.summary()
+    
+    return model
+
+
 def main():
     # load train/validation/test data splits
     X_train,X_validation,X_test,y_train,y_validation,y_test = getDataSplits(DATA_PATH)
     
     # build the CNN model
-    input_data = (X_train.shape[1],X_train.shape[2],X_train.shape[3]) # (num of segemnts,num of coeff (13), num of depth channels (1))
+    input_shape = (X_train.shape[1],X_train.shape[2],X_train.shape[3]) # (num of segemnts,num of coeff (13), num of depth channels (1))
     model = buildModel(input_shape, LEARNING_RATE)
     
     # train the model
